226a16c6c4b37fb2c3479c1933d595df72d230e5
riships <avviare.rishi@gmail.com>
2023-10-23 16:04:47 +0530

    react with java

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Login from './components/login';
 import 'bootstrap/dist/css/bootstrap.min.css';
+// import InfoAboutWeb from './components/InfoAboutWeb'
 
 function App() {
+
     return (
         <>
             <Login/>
--- client/src/components/InfoAboutWeb.jsx
+++ client/src/components/InfoAboutWeb.jsx
@@ -1,33 +1,49 @@
 import React, {useState} from 'react';
 import {Button} from "react-bootstrap";
-
-// import validator from "validator";
+import validator from "validator";
 
 function InfoAboutWeb() {
-
+    const [error, setError] = useState(false);
     const [info, setInfo] = useState({
-        firstName: "",
+        title: "",
         lastName: "",
         age: "",
         email: ""
     });
+    const errors = {
+        title: "Please enter title",
+        icon: "Please upload your icon"
+    };
 
-    const [step, setstep] = useState(1);
+    const [step, setStep] = useState(1);
 
     const submitFormData = (e) => {
         e.preventDefault();
 
 
+        // checking if value of first name and last name is empty show error else take to step 2
+        if (
+            validator.isEmpty(info.title)
+        ) {
+            setError(true);
+        }else if(
+            validator.isEmpty(info.title)
+        ){}
+        else {
+            document.title = info.title;
+            nextStep();
+        }
     };
+
 
     // function for going to next step by increasing step state by 1
     const nextStep = () => {
-        setstep(step + 1);
+        setStep(step + 1);
     };
 
     // function for going to previous step by decreasing step state by 1
     const prevStep = () => {
-        setstep(step - 1);
+        setStep(step - 1);
     };
 
     // handling form input data by taking onchange value and updating our previous form data state
@@ -42,18 +58,30 @@ function InfoAboutWeb() {
         }));
     }
 
+
     // javascript switch case to show different form in each step
     switch (step) {
         case 1:
             return (
-                <>
-                    first step
-                    <Button variant="primary" type="submit"
-                            onClick={nextStep}
-                    >
+
+                <form onSubmit={submitFormData}>
+                    <div className="input-container">
+                        <label>Title</label>
+                        <input
+                            type="text"
+                            name="Title"
+                            value={info.title}
+                            onChange={handleInputData("title")}
+                            required
+                        />
+
+                        {/*{renderErrorMessage("title")}*/}
+                    </div>
+                    <Button variant="primary" type="submit">
                         Continue
                     </Button>
-                </>
+                </form>
+
             );
         case 2:
             return (
