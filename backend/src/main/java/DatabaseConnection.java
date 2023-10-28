71e947153000ffbe17f03f4277bb2c28a6ef4501
riships <avviare.rishi@gmail.com>
2023-10-28 17:08:05 +0530

    react with java

--- backend/src/main/java/DatabaseConnection.java
+++ backend/src/main/java/DatabaseConnection.java
@@ -7,13 +7,12 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     public static void main(String args[]) throws SQLException {
         try {
-            // Load the MySQL JDBC driver
-//            class.("com.mysql.jdbc.Driver");
             Connection connection = null;
             String url = "jdbc:mysql://localhost:3306/dynamicdata";
             String user = "root";
             String password = "hrhk";
             connection = DriverManager.getConnection(url, user, password);
+            System.out.println(connection);
         } catch (SQLException e) {
             e.printStackTrace();
         }
--- backend/src/main/java/com/MyServlet.java
+++ backend/src/main/java/com/MyServlet.java
@@ -6,15 +6,32 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
 public class MyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // Handle GET requests
+        System.out.println("Hello world");
+        request.getRequestDispatcher("/login.jsp").forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // Handle POST requests
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        boolean isAuthenticated = authenticateUser(username, password);
+        if (isAuthenticated) {
+            // Store user information in a session
+            HttpSession session = request.getSession();
+            session.setAttribute("username", username);
+
+            // Send a success response
+            response.setStatus(HttpServletResponse.SC_OK);
+        }
+        else {
+            // Send an error response
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        }
     }
 }
 
