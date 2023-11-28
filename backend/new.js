d400228d8c10fe1be86033d4f4814535a33a4dcc
riships <avviare.rishi@gmail.com>
2023-11-28 17:23:07 +0530

    crud in node

--- backend/new.js
+++ backend/new.js
@@ -23,8 +23,44 @@
 // console.log(__dirname);
 
 const path = require('path');
-console.log(path);
+const fs = require('fs');
+const dirPath = path.join(__dirname, 'crud');
+const filePath = `${dirPath}/apple.txt`
 
+// this is the func for add and write the content in file
+// fs.writeFileSync(filePath, 'This is sample for creating files using fs');
+
+// this is for the reading the content of the file
+// fs.readFile(filePath, 'utf8', (err, item) => {
+//     console.log(item);
+// })
+
+
+// this is the func or method to add the content to the file whih we have already created
+
+// fs.appendFile(filePath, ' and i am updating the content for my test case', (err) => {
+//     if (!err) {
+//         console.log('file is updated');
+//     }
+// })
+
+// this is  the func or method to rename the file
+
+// fs.rename(filePath, `${dirPath}/myDemo.txt`, (err) => {
+//     if (!err) {
+//         console.log('File Renamed');
+//     }
+// })
+
+// this is the method to delete the file
+
+// fs.unlinkSync(`${dirPath}/myDemo.txt`, (err) => {
+//     if (!err) {
+//         console.log('The file is deleted');
+//     } else {
+//         console.log('This is an error');
+//     }
+// });
 
 
 
