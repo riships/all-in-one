86a5c1f4bbddf60f5e8910f1d7f9bc07f3766a6d
riships <avviare.rishi@gmail.com>
2023-12-19 23:02:16 +0530

    first_deploye_hello_api

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,12 @@
-const express = require('express');
-const app = express();
 
-const reqFilter = (req, res, next) => {
-    if (!req.query.age) {
-        res.send('Please provide your age');
-    } else if (req.query.age < 18) {
-        res.send('You are not authorize to access this page')
-    }
-    else {
-        next();
-    }
-};
-
-// app.use(reqFilter);
-
+const express = require('express')
+const app = express()
+const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the world of node js home');
-});
+    res.send('Hello World!')
+})
 
-app.get('/user', reqFilter, (req, res) => {
-    res.send('Welcome to the world of node js user');
-});
-
-let port = 5000;
-app.listen(port);
-
-
-console.log(`server is running on http://localhost:${port}`);
-
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
